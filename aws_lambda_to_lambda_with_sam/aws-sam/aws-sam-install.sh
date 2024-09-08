998aea1f3f75e25e4c8489d4eb17c80eef73225c
Srinivasa Rao Gurram <srinivasarao.gurram@gmail.com>
2024-09-07 19:50:33 -0500

    adding saml cli for repo

--- aws_lambda_to_lambda_with_sam/aws-sam/aws-sam-install.sh
+++ aws_lambda_to_lambda_with_sam/aws-sam/aws-sam-install.sh
@@ -1,7 +1,2 @@
-curl "https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip" -o "aws-sam-cli-linux-x86_64.zip"
-
-curl -o aws-sam-cli-linux-x86_64.zip  https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
-
-unzip aws-sam-cli-linux-x86_64.zip
-
+sudo ./aws-sam-cli-linux-x86_64/install
 sam --version
